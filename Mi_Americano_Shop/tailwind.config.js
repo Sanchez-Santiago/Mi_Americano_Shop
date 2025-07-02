4c1aa661fcbef25e9461be50abf9946011c75c0a
Santiago Sanchez <ssanchez321@alumnos.iua.edu.ar>
2025-07-02 10:40:44 -0300

    Inicio del proyecto

---
+++ Mi_Americano_Shop/tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./src/**/*.{astro,js,ts,jsx,tsx,html}"],
+    theme: {
+      extend: {
+        colors: {
+          primario: "#FF008E",     // Azul fuerte
+          secundario: "#D22779",   // Amarillo
+          tercero: "#612897",      // Rojo
+          cuarto: "#0C1E7F",       // Gris claro (como bg-gray-100)
+        },
+      },
+    },
+    plugins: [],
+  }
+  

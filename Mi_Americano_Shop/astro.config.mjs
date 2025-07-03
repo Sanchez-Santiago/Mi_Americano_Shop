0ad2489ef3d88b5662b8f8909458682245532898
Santiago Sanchez <ssanchez321@alumnos.iua.edu.ar>
2025-07-02 23:55:16 -0300

    Login y creacion del midelware

--- Mi_Americano_Shop/astro.config.mjs
+++ Mi_Americano_Shop/astro.config.mjs
@@ -1,6 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
+import { neobrutalism } from '@clerk/themes'
+import { esES } from '@clerk/localizations'
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [clerk(
+    {
+      localization: esES,
+      appearance: {
+        baseTheme: neobrutalism,
+      },
+    }
+  ), tailwind()],
+  adapter: node({ mode: "standalone" }),
+  output: "server",
 });
